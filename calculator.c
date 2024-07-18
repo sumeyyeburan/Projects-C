68679619476a686149524e124af84356284c962b
Sumeyye Buran <sumeyye.b0123@gmail.com>
2024-07-18 13:12:13 +0300

    Calculator

--- calculator.c
+++ calculator.c
@@ -2,91 +2,92 @@
 #include <math.h>
 
 void add(double x, double y) {
-	printf("\n%.2lf + %.2lf = %.2lf\n\n", x, y, x + y);
+    printf("\n%.2lf + %.2lf = %.2lf\n\n", x, y, x + y);
 }
 
 void sub(double x, double y) {
-	printf("\n%.2lf - %.2lf = %.2lf\n\n", x, y, x - y);
+    printf("\n%.2lf - %.2lf = %.2lf\n\n", x, y, x - y);
 }
 
 void mult(double x, double y) {
-	printf("\n%.2lf * %.2lf = %.2lf\n\n", x, y, x * y);
+    printf("\n%.2lf * %.2lf = %.2lf\n\n", x, y, x * y);
 }
 
 void div(double x, double y) {
-	if (y == 0)
-		printf("\nGecersiz islem, sifira bolunemez hatasi!\n\n");
-	else
-		printf("\n%.2lf / %.2lf = %.2lf\n\n", x, y, x / y);
+    if (y == 0)
+        printf("\nInvalid operation, division by zero error!\n\n");
+    else
+        printf("\n%.2lf / %.2lf = %.2lf\n\n", x, y, x / y);
 }
 
 void power(double x, double y) {
-	printf("\n%.2lf ^ %.2lf = %.2lf\n\n", x, y, pow(x, y));
+    printf("\n%.2lf ^ %.2lf = %.2lf\n\n", x, y, pow(x, y));
 }
 
 void mod(double x, double y) {
-	if (y == 0)
-		printf("\nGecersiz islem, sifira bolunemez hatasi!\n\n");
-	else {
-		double result = fmod(x, y);
-		printf("\n%.2lf %% %.2lf = %.2lf\n\n", x, y, result);
-	}
+    if (y == 0)
+        printf("\nInvalid operation, division by zero error!\n\n");
+    else {
+        double result = fmod(x, y);
+        printf("\n%.2lf %% %.2lf = %.2lf\n\n", x, y, result);
+    }
 }
 
 int main(void) {
-	int choice;
-	double num1, num2;
+    int choice;
+    double num1, num2;
 
-	printf("\n****** HESAPLAMAYA HAZIRIM ! *******\n");
-	printf("\n\n\n");
-	printf("Oncelikle yapmak istediginiz islemi seciniz...\n");
-	while (1) {  //sonsuz d?ng?
+    printf("\n****** READY TO CALCULATE ! *******\n");
+    printf("\n\n\n");
+    printf("First, choose the operation you want to perform...\n");
+    while (1) {  // infinite loop
 
-		printf("\n1. Toplama\n");
-		printf("2. Cikarma\n");
-		printf("3. Carpma\n");
-		printf("4. Bolme\n");
-		printf("5. Us Alma\n");
-		printf("6. Mod Alma\n");
-		printf("7. Cikis\n");
-		printf("\nSeciminiz: ");
-		scanf("%d", &choice);
+        printf("\n1. Addition\n");
+        printf("2. Subtraction\n");
+        printf("3. Multiplication\n");
+        printf("4. Division\n");
+        printf("5. Exponentiation\n");
+        printf("6. Modulus\n");
+        printf("7. Exit\n");
+        printf("\nYour choice: ");
+        scanf("%d", &choice);
 
-		if (choice == 7) {
-			printf("\n\n****** HESAP MAKINESI SONLANDI ! ******\n\n");
-			break;
-		}
-		if (choice < 1 || choice>7) {
-			printf("\n\nGecersiz secim, tekrar deneyiniz!\n");
-			continue;
-		}
+        if (choice == 7) {
+            printf("\n\n****** CALCULATOR TERMINATED ! ******\n\n");
+            break;
+        }
+        if (choice < 1 || choice > 7) {
+            printf("\n\nInvalid choice, please try again!\n");
+            continue;
+        }
 
-		printf("\nIslemini yapmak istediginiz sayilari giriniz...\n");
-		printf("Sayi-1: ");
-		scanf("%lf", &num1);
-		printf("Sayi-2: ");
-		scanf("%lf", &num2);
+        printf("\nEnter the numbers you want to operate on...\n");
+        printf("Number-1: ");
+        scanf("%lf", &num1);
+        printf("Number-2: ");
+        scanf("%lf", &num2);
 
-		switch (choice) {
-		case 1:
-			add(num1, num2);
-			break;
-		case 2:
-			sub(num1, num2);
-			break;
-		case 3:
-			mult(num1, num2);
-			break;
-		case 4:
-			div(num1, num2);
-			break;
-		case 5:
-			power(num1, num2);
-			break;
-		case 6:
-			mod(num1, num2);
-			break;
-		}
-	}
-	return 0;
+        switch (choice) {
+        case 1:
+            add(num1, num2);
+            break;
+        case 2:
+            sub(num1, num2);
+            break;
+        case 3:
+            mult(num1, num2);
+            break;
+        case 4:
+            div(num1, num2);
+            break;
+        case 5:
+            power(num1, num2);
+            break;
+        case 6:
+            mod(num1, num2);
+            break;
+        }
+    }
+    return 0;
 }
+
