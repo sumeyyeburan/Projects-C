317e234692f79f16657eec0f9e833bde02d633eb
Sumeyye Buran <sumeyye.b0123@gmail.com>
2024-07-18 13:13:34 +0300

    Faktorial

--- factorial.c
+++ factorial.c
@@ -1,26 +1,26 @@
 #include <stdio.h>
 
 int factorial(int f){
-	
-	if(f==0 || f==1)
-		return 1;
-	
-	return f*factorial(f-1);
+    if(f == 0 || f == 1)
+        return 1;
+    
+    return f * factorial(f - 1);
 }
 
 int main(void){
-	int fac;
-	
-	again:
-	printf("\nFaktoriyelinin hesaplanmasini istediginiz sayiyi giriniz:\n");
-	scanf("%d",&fac);
-	
-	if(fac<0){
-		printf("\n%d sayisinin faktoriyeli hesaplanamaz,tekrar deneyiniz!\n\n",fac);
-		goto again;
-	}
-	
-	printf("\n%d! = %d\n",fac,factorial(fac));
-	
-	return 0;
+    int fac;
+    
+    again:
+    printf("\nEnter the number for which you want to calculate the factorial:\n");
+    scanf("%d", &fac);
+    
+    if(fac < 0){
+        printf("\nThe factorial of %d cannot be calculated, please try again!\n\n", fac);
+        goto again;
+    }
+    
+    printf("\n%d! = %d\n", fac, factorial(fac));
+    
+    return 0;
 }
+
